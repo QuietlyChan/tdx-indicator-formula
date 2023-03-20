a429f169510b89509c5287297ed915633bf7ed09
QuietlyChan <quietlychan@foxmail.com>
2023-03-20 12:22:42 +0800

    增加正则高亮

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,12 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 import { keywords } from "./complete/keyword";
+
+let tmp = "";
+keywords.map(item=>{
+  tmp+=item.label+"|";
+});
+console.log(tmp);
 import {
   createConnection,
   TextDocuments,
